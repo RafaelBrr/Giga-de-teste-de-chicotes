dcaa625dff4461d852be982db89dc41db3389d3d
fmce508 <rafaelsouza.fmce@imbel.gov.br>
2025-12-12 16:55:50 -0300

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 
-#include "U8g2lib.h"
+#include <U8g2lib.h>
 
 #include "Functions.h"
-
-#include "Menu.h"
+#include <Menu.h>
 
 // --- Display LCD ST7920 (modo SPI via software) ---
 U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /*clk=*/52, /*data=*/51, /*cs=*/53, /*reset=*/U8X8_PIN_NONE);
@@ -27,6 +26,13 @@ const int rxPins[8] = {30, 31, 32, 33, 34, 35, 36, 37}; // Entradas
 int menuIndex = 0;
 const char* menuItems[] = {"Iniciar Teste", "Ver Resultados", "Reset", "Sobre"};
 const int menuSize = sizeof(menuItems) / sizeof(menuItems[0]);
+
+void realizarTeste();
+void mostrarResultados();
+void resetarSistema();
+void mostrarSobre();
+void drawMenu();
+
 
 // --- Estado dos botões (debounce simples) ---
 bool btnPressed(int pin) {
@@ -61,7 +67,6 @@ void setup() {
 }
 
 
-
 void loop() {
   drawMenu();
 
@@ -94,15 +99,82 @@ void loop() {
   }
 }
 
+void realizarTeste() {
+  bool sucesso = true;
 
-//********************************************************************************************************************************** */
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tr);
+  u8g2.drawStr(0, 12, "Executando Teste...");
+  u8g2.sendBuffer();
 
+  delay(500);
 
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(txPins[i], HIGH);
+    delay(10);
 
+    for (int j = 0; j < 8; j++) {
+      int val = digitalRead(rxPins[j]);
 
+      if (val == HIGH) {
+        if (i == j) {
+          Serial.print("Linha ");
+          Serial.print(i);
+          Serial.println(": OK");
+        } else {
+          Serial.print("Erro! Curto entre ");
+          Serial.print(i);
+          Serial.print(" e ");
+          Serial.println(j);
+          sucesso = false;
+        }
+      }
+    }
 
+    digitalWrite(txPins[i], LOW);
+  }
 
+  if (sucesso) {
+    digitalWrite(LED_VERDE, HIGH);
+    digitalWrite(LED_VERMELHO, LOW);
+    u8g2.drawStr(0, 30, "RESULTADO: OK");
+  } else {
+    digitalWrite(LED_VERDE, LOW);
+    digitalWrite(LED_VERMELHO, HIGH);
+    u8g2.drawStr(0, 30, "ERROS DETECTADOS");
+  }
 
+  u8g2.sendBuffer();
+  delay(2000);
+}
 
+void mostrarResultados() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tr);
+  u8g2.drawStr(0, 12, "Resultados via Serial");
+  u8g2.drawStr(0, 24, "Consulte LabVIEW.");
+  u8g2.sendBuffer();
+  delay(2000);
+}
 
+void resetarSistema() {
+  digitalWrite(LED_VERDE, LOW);
+  digitalWrite(LED_VERMELHO, LOW);
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tr);
+  u8g2.drawStr(0, 24, "Sistema Resetado");
+  u8g2.sendBuffer();
+  delay(2000);
+}
 
+void mostrarSobre() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tr);
+  u8g2.drawStr(0, 12, "Jiga de Teste IMBEL");
+  u8g2.drawStr(0, 24, "Versao: 1.0");
+  u8g2.drawStr(0, 36, "Arduino Mega Pro Mini");
+  u8g2.sendBuffer();
+  delay(2500);
+}
+
+//*********************************************************************************************************************************************************************** */
